132e5407374f2af8dd178b16e0c245533afd9ad1
prikkie <prikantuma@hotmail.com>
2019-12-17 15:50:09 +0100

    Init

--- default.php
+++ default.php
@@ -1,4 +1,55 @@
 <?php
-include "includes/db_connect.php";
-echo "hello!";
+session_start();
+require 'files/functions/db_connect.php';
+include "files/winkelmandInit.php";
+include "functions/winkelmandVerschil.php";
 
+$cart_products = initCart($conn);
+
+//Kijkt welke pagina opgevraagd is, kijkt of het bestaat, veranderd $pagina. Bestaat deze niet? Gaat de gebruiker naar de home pagina
+if (isset($_GET['nav']) && file_exists('files/' . $_GET['nav'] . '.php')) {
+	$pagina = $_GET['nav'];
+} else {
+	header("Location: http://projecthanze.com/home");
+}
+
+echo "<div id='header'>";
+include 'files/header.php';
+echo "</div>";
+
+if ($pagina == "home") {
+} else {
+
+	echo "<div id='menu'>";
+	include 'files/menu.php';
+
+	echo "</div>";
+
+
+}
+?>
+    <div id="body">
+		<?php
+		if ($pagina == "home" || $pagina == "signup" || $pagina == "checkout" || $pagina == "account" || $pagina == "orders" || $pagina == "bedankt" || $pagina == "spaarpunten") {
+		} else {
+
+			echo "<div id='winkelmandje'>";
+			include 'files/winkelmandView.php';
+
+			echo "</div>";
+		}
+		// Als $pagina niet geinitialiseerd is, gaat hij tergu naar home. Anders geeft hij de $pagina weer.
+		if (empty($pagina)) {
+			header("Location: http://projecthanze.com/home");
+		} else {
+			include("files/" . $pagina . ".php");
+		}
+		?>
+    </div>
+<?php
+
+
+?>
+    <div class="footer"><?php include 'files/footer.php'; ?></div>
+<?php
+?>
---
+++ files/login.php
@@ -0,0 +1,26 @@
+<?php
+function login($gebruikersnaam, $wachtwoord)
+{
+	global $conn;
+	$user_ip = (sha1($_SERVER['REMOTE_ADDR']));
+	$session_id = rand(1000, 1000000);
+	$query = "SELECT * FROM users WHERE gebruikersnaam = '" . $gebruikersnaam . "' AND wachtwoord = '" . $wachtwoord . "' ";
+
+	mysqli_prepare($conn, $query);
+	$result = mysqli_query($conn, $query) or die ("FOUT: " . mysqli_error($conn));
+
+
+	$total = mysqli_num_rows($result);
+
+	if ($total == 1) {
+		foreach ($result as $row) {
+			$_SESSION['account_id'] = $row['id'];
+			$_SESSION['naam'] = $row['naam'];
+		}
+		$_SESSION['session_ip'] = $user_ip;
+		$_SESSION['session_id'] = $session_id;
+		$_SESSION['gebr'] = $gebruikersnaam;
+		$_SESSION['logged_in'] = true;
+
+	}
+}
