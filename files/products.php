5914f4c1721a4ca825e92be761b8bf932aca8c54
prikkie <prikantuma@hotmail.com>
2019-12-20 15:45:46 +0100

    added + and -

--- files/products.php
+++ files/products.php
@@ -4,5 +4,8 @@ $query = 'Select * from products';
 $result = mysqli_query($conn, $query) or die ("FOUT: " . mysqli_error($conn));
 foreach ($result as $row) {
 	echo $row['id'] . " " . $row['price'] . "<br>";
+	echo "<img src='../images/plus.png'>";
+	echo "<img src='../images/min.png'>";
+	echo "<br><br>";
 
 }
